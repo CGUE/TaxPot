d141f76d2b4f7e3e991072d714ed6460ea745557
Jayson Asenjo <mark.jayson.asenjo@live.de>
2017-06-02 11:12:29 +0200

    toast after rating submitted

--- TaxPot/app/src/main/java/ac/at/tuwien/mse/taxpot/fragments/RatingsFragment.java
+++ TaxPot/app/src/main/java/ac/at/tuwien/mse/taxpot/fragments/RatingsFragment.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
@@ -164,6 +165,7 @@ public class RatingsFragment extends Fragment {
                             occupancyRef.setValue(taxPot.getOccupancy());
                         }
                         popupWindow.dismiss();
+                        Toast.makeText(getActivity().getApplicationContext(), getResources().getText(R.string.rating_submitted), Toast.LENGTH_LONG).show();
                         getFragmentManager().popBackStack();
                     }
                 });
